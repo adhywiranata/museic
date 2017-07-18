ce8510ea3fa13c909e2131163724e19a4a39d073
Adhy <adhywiranatap@gmail.com>
2017-07-18 12:53:46 +0700

    modify react-router to use react-router-redux's connectedRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+
+import store from './store/configureStore';
 
 import './App.css';
 import {
@@ -13,22 +18,26 @@ import {
 } from './containers';
 import { Header } from './components';
 
+const history = createHistory();
+
 const App = () => (
-  <BrowserRouter>
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/new-releases" component={NewReleases} />
-        <Route path="/me" component={Profile} />
-        <Route path="/artists" component={TopArtists} />
-        <Route path="/artist/:artistId" component={MyArtists} />
-        {/* <Route path="/callback/:access_token/:refresh_token" component={AuthSuccess} /> */}
-        <Route path="/callback" component={AuthSuccess} />
-      </Switch>
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/new-releases" component={NewReleases} />
+          <Route path="/me" component={Profile} />
+          <Route path="/artists" component={TopArtists} />
+          <Route path="/artist/:artistId" component={MyArtists} />
+          {/* <Route path="/callback/:access_token/:refresh_token" component={AuthSuccess} /> */}
+          <Route path="/callback" component={AuthSuccess} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
+  </Provider>
 );
 
 export default App;
